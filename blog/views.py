d2c3f0cfbffb014e07ccd1fb4b397bbc6105bd7f
Joao Tinti <joaotinti75@gmail.com>
2021-06-06 20:31:27 -0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'blog/index.html')
+
+def blog(request):
+    return render(request, 'blog/blogs.html')
